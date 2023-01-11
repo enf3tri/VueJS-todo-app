e2278d2e5806b7961b202821f03b9a253a4a907c
Mert Ünver <unvermert@windowslive.com>
2023-01-11 19:48:18 +0300

    .

--- VueJS-todo-app/src/App.vue
+++ VueJS-todo-app/src/App.vue
@@ -28,16 +28,25 @@
 </script>
 
 <template>
-    <form @submit.prevent="addTodo">
-
-        <div class="call-button-container">
-            <input v-model="newTodo">
-        </div>
-        <div class="call-button-container">
-            <button>Add</button>
-        </div>
-    </form>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <div class="background-image"></div>
+    <header class="w3-display-container w3-content w3-center" style="max-width:1500px">
+        <div class="w3-bar w3-transparent w3-round w3-display-bottommiddle w3-hide-small" style="bottom:-32px">
+            <form @submit.prevent="addTodo">
+                <div>
+                    <input v-model="newTodo" class="call-button-container">
+                    <div class= "blank"></div>
+                    <button class="add-button">Add</button>
+                </div>
+
+
+
+
+</form>
+        </div>
+    </header>
     <ul>
         <li v-for="todo in filteredTodos" :key="todo.id">
             <input type="checkbox" v-model="todo.done">
@@ -62,28 +71,44 @@
     }
     .call-button-container {
         margin: auto;
-        width: 233px;
+        width: 643px;
         height: 74px;
         vertical-align: middle;
-        padding: 10px;
-        background: #29A3D8;
-        -webkit-border-radius: 4px;
-        -moz-border-radius: 4px;
-        border-radius: 20px;
+        padding: 2px;
+        background: white;
+        border-radius: 36px;
         font-family: lato-bold;
-        font-size: 17px;
-        color: white;
+        font-size: 38px;
+        border: 2px solid rgba(0, 0, 0, 0.1);
+        background-color: white;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     }
     .background-image {
         content: url(https://i.hizliresim.com/2bs63np.png);
         width: 100%;
-        height: 500px;
+        height: 480px;
         object-fit: cover;
     }
     .input_style {
-        border-radius: 20px;
-        height: 200px;
-        color: cornflowerblue;
+        border-radius: 25px;
+        background: #73AD21;
+        padding: 20px;
+        width: 200px;
+        height: 150px;
+    }
+    .add-button {
+        margin: auto;
+        height: 74px;
+        width: 74px;
+        color: black;
+        border-radius: 37px;
+        background: black;
 
+    }
+    .blank {
+        margin: auto;
+        height: 74px;
+        width: 2px;
+        color: black;
     }
 </style>
